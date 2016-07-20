f860284ebe950245a800458915e8d298c2c655f8
Asier Fernandez <asier.fernandez@tecnalia.com>
2016-07-20 11:13:22 +0200

    add basic code to inheritance actual ros node. It only compiles

---
+++ optoforce_node/src/optoforce_topic.cpp
@@ -0,0 +1,18 @@
+#include "optoforce_topic.h"
+
+optoforce_topic::optoforce_topic()
+{
+
+}
+
+optoforce_topic::~optoforce_topic()
+{
+
+}
+
+int main_topic(int argc, char* argv[])
+{
+  optoforce_topic of_topic;
+
+  return 1;
+}
---
+++ optoforce_node/src/optoforce_topic.h
@@ -0,0 +1,13 @@
+#include "optoforce_node.h"
+
+class optoforce_topic : optoforce_node {
+//class optoforce_topic {
+
+  public:
+
+    optoforce_topic();
+    ~optoforce_topic();
+
+  //virtual void transmit ();
+
+};
